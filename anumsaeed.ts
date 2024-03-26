4255bc8e1bf946cf9e2d6db1fd44d1f5d7aa3917
Meilianum <164498186+Meilianum@users.noreply.github.com>
2024-03-26 03:52:46 -0700

    Add files via upload

---
+++ anumsaeed.ts
@@ -0,0 +1,112 @@
+// Arthemetic Operator
+// let  a = 5
+// let b = 2 
+
+// console.log(" a + b " , a + b );//7
+// console.log(" a - b ", a - b  ); ///3
+// console.log(" a * b ", a * b  ); //10 
+// console.log(" a / b ", a / b  ); //2.5
+// console.log(" a % b ", a % b  ); // 1
+// console.log(" a ** b ", a ** b );//25
+ 
+
+// //Unary Operators
+
+// let  c = 5
+// let d = 4 
+// ++c;
+// --d ;
+// console.log(c ) // 5
+//  console.log( d ) // 4
+// c ++ ;
+// d -- ;
+//  console.log(c ) // 6
+//  console.log( d ) // 3*/
+
+//  // Assigment Operators
+
+// let  a = 6
+//  let b =  2
+//  //a += 4;
+//  //a -= 4
+//  //a *= 4
+//  //a /= 4
+//  //a %= 4
+//  a **= 4
+//  console.log("a =" , a); 
+
+//  //comparsion opertors
+
+//  let a = 4
+//  let b = 2
+//  console.log(" a == b " , a == b);
+//  console.log(" a != b " , a != b);
+//  console.log(" a === b " , a === b);
+//  console.log(" a > b " , a > b);
+//  console.log(" a < b " , a < b);
+
+//  // Logical Operators
+
+//  let he = 155
+//  let wi = 40 
+// let cond1 = he < wi 
+//  let cond2 =  he === 155
+//  //console.log(" cond1 && cond2 ", cond1 && cond2 )
+//  //console.log(" cond1 || cond2 ", cond1 || cond2 )
+//  console.log("!(cond1 || cond2)  ", !(cond1 || cond2 ));
+
+//  // Conditinal Statament ;
+
+// let age = 23
+//  if ( age > 18 ){
+//     console.log( "you can vote" )
+//  }
+
+//  let mode = "light ";
+//  let color ;
+//  if ( "mode === dark") {
+//     color = "black"
+//  } else {
+//     color = "white "
+//  }
+// console.log(color);
+
+
+// // Odd Or Even 
+
+// let num = 7;
+// if ( num% 2 === 0 ){
+//     console.log( "even")
+// }else{
+//     console.log(" odd")
+// }
+
+// let mode = " dark ";
+// let color ;
+// if( mode === "dark"){
+//     color = "black"
+// } else if ( mode === "pink" ){
+//     color = "red"
+// } else if ( mode ===" light "){
+//     color = "white"
+// } else {
+//     color = "white"
+// }
+// console.log( color)
+
+// import marquestStudents from "./berege.js";
+
+// const laptopStudent: number = marquestStudents();
+// console.log( laptopStudent);
+
+
+// // function in Typescript
+ 
+//   for (let i = 1; i <=5 ;i ++) {
+//     console.log( "Anum saeed");
+//   }
+//   function Maggie ( masla :number , water: number , maggie :number ): number{
+//      return masla+water+maggie
+//      }
+//      let response : number=Maggie (1 ,2.5 ,3)
+//      console.log(response);
